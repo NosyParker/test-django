e8b12b4b9f95552358d9b66df58e7e09e0fa498e
Roman Vasilyev <sektor_wins@mail.ru>
2017-10-27 23:31:38 +0500

    Hotfix: добавлена в админку модель "заказчик"

--- food_delivery/apps/core_app/admin.py
+++ food_delivery/apps/core_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Meal, Order, OrderDetails
+from .models import Meal, Order, OrderDetails, Customer
 
 admin.site.register(Meal)
 admin.site.register(Order)
 admin.site.register(OrderDetails)
+admin.site.register(Customer)
