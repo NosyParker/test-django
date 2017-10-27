c7890469ae13fc5953f61c83797af1bb5aa8242f
Roman Vasilyev <sektor_wins@mail.ru>
2017-10-27 22:59:12 +0500

    Добавлены модели (Customer, Meal, Order, OrderDetails)

--- food_delivery/apps/core_app/models.py
+++ food_delivery/apps/core_app/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Meal(models.Model): # модель - "Блюдо"
+    meal_name = models.CharField(max_length=140)
+    description = models.CharField(max_length=500)
+    cost = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.meal_name
+
+
+class Customer(models.Model): #модель - "Заказчик"
+    user_name = models.CharField(max_length=140)
+    phone = models.CharField(max_length=140, blank=True)
+    address = models.CharField(max_length=500, blank=True)
+
+    def __str__(self):
+        return self.user_name
+
+
+class Order(models.Model): #модель - "Заказ"
+    NEW_ORDER = 1
+    WORKING = 2
+    DONE = 3
+
+    ORDER_STATUS = (
+        (NEW_ORDER, 'Новый заказ'), (WORKING, 'В работе'), (DONE, 'завершен')
+        )
+    
+    customer = models.ForeignKey(Customer)
+    status = models.IntegerField(order_status=ORDER_STATUS)
+    address = models.CharField(max_length=500)
+
+
+class OrderDetails(models.Model): #модель - "Детализация заказа"
+    order = models.ForeignKey(Order)
+    meal = models.ForeignKey(Meal)
+    quantity = models.IntegerField()
+
+
