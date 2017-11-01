384510c2c3bad58f826f90f3042f9af68c5a458b
Roman <sektor_wins@mail.ru>
2017-11-02 01:16:16 +0500

    Upd: Новый вариант системы регистрации заказчиков

--- food_delivery/apps/core_app/forms.py
+++ food_delivery/apps/core_app/forms.py
@@ -3,12 +3,23 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import Customer, Meal
 
-class UserForm(forms.ModelForm):
 
-    email = forms.CharField(max_length=140, required=True)
+
+class RegistrationForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
+        model = User
+        # fileds=("email","username","first_name","last_name","password") ----так должно работать
+
+        #но вместо этого приходится писать ----
+
+        exclude=["last_login", "user_permissions","is_staff","is_active","date_joined","groups","is_superuser"] #ЭТОГО ЗДЕСЬ НЕ ДОЛЖНО БЫТЬ
+
+class CustomerSignUpForm(forms.ModelForm):
+
+    class Meta:
         model = Customer
-        fields = ("phone", "address")
+        # fields = ("phone","address") аналогично с верхней ситуацией
+        exclude=["user"] # И ЭТОГО ТОЖЕ
 
--- food_delivery/apps/core_app/views.py
+++ food_delivery/apps/core_app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.http import HttpResponse
-from .models import Meal,Customer
-from .forms import UserForm
+from .models import Meal, Customer
+from django.contrib.auth import login, authenticate
+from .forms import CustomerSignUpForm, RegistrationForm
 
 def home(request):
     random_meals = Meal.objects.order_by('?')[:4]
@@ -11,17 +12,18 @@ def home(request):
 
 
 def system_sign_up(request):
-    if request.method=="POST":
-        user_form = UserForm(request.POST)
-        if user_form.is_valid():
-            customer = user_form.save(commit=False)
-            customer.user_name = request.POST['username']
-            customer.phone = request.POST['phone']
-            customer.address = request.POST['address']
-            customer.save()
+    if request.method == 'POST':
+        register = RegistrationForm(request.POST, prefix='register')
+        user_profile = CustomerSignUpForm(request.POST, prefix='profile')
 
-        
+        if register.is_valid() and user_profile.is_valid():
+            user = register.save()
+            usrprof = user_profile.save(commit=False)
+            usrprof.user = user
+            usrprof.save()
+            return HttpResponse('Congrats!!!')
+    
     else:
-        user_form = UserForm()
-
-    return render(request, 'core_app/sign_up.html', {'user_form':user_form})
+        userform = RegistrationForm(prefix='register')
+        userprofileform = CustomerSignUpForm(prefix='profile')
+        return render(request, 'core_app/sign_up.html', {'userform': userform, 'userprofileform': userprofileform})
