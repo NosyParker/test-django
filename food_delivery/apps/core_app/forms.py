5789d61135afd2ab0c4a998030a35abbb5ac7309
Roman <sektor_wins@mail.ru>
2017-11-02 02:06:27 +0500

    Fix: исправлена форма регистрации и соотвествующее view

--- food_delivery/apps/core_app/forms.py
+++ food_delivery/apps/core_app/forms.py
@@ -6,7 +6,8 @@ from .models import Customer, Meal
 
 
 class RegistrationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
 
     class Meta:
         model = User
@@ -15,6 +16,7 @@ class RegistrationForm(forms.ModelForm):
         #но вместо этого приходится писать ----
 
         exclude=["last_login", "user_permissions","is_staff","is_active","date_joined","groups","is_superuser"] #ЭТОГО ЗДЕСЬ НЕ ДОЛЖНО БЫТЬ
+
 
 class CustomerSignUpForm(forms.ModelForm):
 
--- food_delivery/apps/core_app/views.py
+++ food_delivery/apps/core_app/views.py
@@ -5,22 +5,33 @@ from .models import Meal, Customer
 from django.contrib.auth import login, authenticate
 from .forms import CustomerSignUpForm, RegistrationForm
 
+
 def home(request):
+    """ Вывод рандомных блюд на главной странице """
+
     random_meals = Meal.objects.order_by('?')[:4]
     return render(request, 'core_app/home.html', {'random_meals':random_meals})
 
 
 
 def system_sign_up(request):
+    """ Регистрация нового пользователя """
+
     if request.method == 'POST':
         register = RegistrationForm(request.POST, prefix='register')
         user_profile = CustomerSignUpForm(request.POST, prefix='profile')
 
         if register.is_valid() and user_profile.is_valid():
-            user = register.save()
+            
+            user = register.save(commit=False)
+            user.set_password(register.cleaned_data['password'])
+            user.save()
+            
             usrprof = user_profile.save(commit=False)
             usrprof.user = user
             usrprof.save()
+
+
             return HttpResponse('Congrats!!!')
     
     else:
