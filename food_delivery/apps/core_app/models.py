71cc8f321c98ea49db9b1361529b8982b3764b23
Roman <sektor_wins@mail.ru>
2017-11-12 15:53:08 +0500

    Upd: models

--- food_delivery/apps/core_app/models.py
+++ food_delivery/apps/core_app/models.py
@@ -2,19 +2,38 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Meal(models.Model): # модель - "Блюдо/Обед"
-    meal_name = models.CharField(max_length=140)
-    description = models.CharField(max_length=500)
-    cost = models.IntegerField(default=0)
+class ComplexDinner(models.Model):
+    dinner_name = models.CharField(max_length=140, verbose_name="Название")
+    cost = models.IntegerField(default=0, verbose_name="Цена")
+    description = models.TextField(verbose_name="Описание")
+
+    def __str__(self):
+        return self.dinner_name
+
+    class Meta:
+        verbose_name = "Обед"
+        verbose_name_plural = "Обеды"
+
+
+class Meal(models.Model): 
+    dinner = models.ForeignKey(ComplexDinner, verbose_name="Входит в состав обеда")
+    meal_name = models.CharField(max_length=140, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание")
+    cost = models.IntegerField(default=0, verbose_name="Цена")
+ 
 
     def __str__(self):
         return self.meal_name
 
+    class Meta:
+        verbose_name = "Блюдо"
+        verbose_name_plural = "Блюда"
 
-class Customer(models.Model): #модель - "Заказчик"
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
-    phone = models.CharField(max_length=140, blank=True, null=True)
-    address = models.CharField(max_length=500, blank=True, null=True)
+
+class Customer(models.Model): 
+    user = models.OneToOneField(User, verbose_name="Пользователь", on_delete=models.CASCADE)
+    phone = models.CharField(max_length=140, verbose_name="Телефон", blank=True, null=True)
+    address = models.CharField(max_length=500, verbose_name="Адрес", blank=True, null=True)
 
     def __str__(self):
         return self.user.username
@@ -22,8 +41,11 @@ class Customer(models.Model): #модель - "Заказчик"
     def save(self, *args, **kwargs):
         super(Customer, self).save(*args, **kwargs)
 
+    class Meta:
+        verbose_name = "Заказчик"
+        verbose_name_plural = "Заказчики"
 
-class Order(models.Model): #модель - "Заказ"
+class Order(models.Model): 
     NEW_ORDER = 1
     WORKING = 2
     DONE = 3
@@ -32,14 +54,18 @@ class Order(models.Model): #модель - "Заказ"
         (NEW_ORDER, 'Новый заказ'), (WORKING, 'В работе'), (DONE, 'завершен')
         )
     
-    customer = models.ForeignKey(Customer)
-    status = models.IntegerField(choices=ORDER_STATUS)
-    address = models.CharField(max_length=500)
+    customer = models.ForeignKey(Customer, verbose_name="Заказчик")
+    status = models.IntegerField(choices=ORDER_STATUS, verbose_name="Статус заказа")
+    address = models.CharField(max_length=500, verbose_name="Адрес")
 
     def __str__(self):
         return str(self.id)
 
-class OrderDetails(models.Model): #модель - "Детализация заказа"
+    class Meta:
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
+
+class OrderDetails(models.Model): 
     order = models.ForeignKey(Order)
     meal = models.ForeignKey(Meal)
     quantity = models.IntegerField()
