39d578feb5c0079b24c98c38811b233dcd3c5ccf
Roman <sektor_wins@mail.ru>
2017-11-18 02:34:58 +0500

    Upd: исправлены url с новым декоратором. Добавлен url для отображения аккаунта. Добавлена настройка для редиректа при логауте

--- food_delivery/apps/core_app/urls.py
+++ food_delivery/apps/core_app/urls.py
@@ -5,12 +5,14 @@ from django.contrib.auth import views as auth_views
 app_name = 'core_app'
 urlpatterns = [
      url(r'^$', views.home, name='home'),
-     url(r'^sign-in/', auth_views.login, 
+     url(r'^sign-in/', views.logout_required(auth_views.login), 
         {"template_name":"core_app/sign_in.html"},
         name = "sign-in"),
-     url(r'^sign-up/', views.sign_up,
+     url(r'^sign-up/', views.logout_required(views.sign_up),
         name = "sign-up"),
      url(r'^logout/', auth_views.logout, 
         {"next_page":"/"},
         name = "logout"),
+     url (r'^account/$', views.account, name="account"),
+
 ]
