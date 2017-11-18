97109dec63dbe7a5e8a3fc1cea74e301528d0735
Roman <sektor_wins@mail.ru>
2017-11-18 19:49:20 +0500

    Upd: добавлены формы для редактирования в меню аккаунта + логика для их обработки в новой вьюшке

--- food_delivery/apps/core_app/forms.py
+++ food_delivery/apps/core_app/forms.py
@@ -5,10 +5,33 @@ from core_app.models import Customer, Meal
 
 
 class UserRegistrationForm(forms.ModelForm):
+    email = forms.CharField(max_length=100, required=True)
+    password=forms.CharField(label="Пароль", widget=forms.PasswordInput(), required=True)
+    confirm_password=forms.CharField(label="Повторите пароль", widget=forms.PasswordInput(), required=True)
+
+    def clean(self):
+        cleaned_data = super(UserRegistrationForm, self).clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.pop("confirm_password", None)
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Пароли не совпадают!"
+            )         
+        return self.cleaned_data
+    
     class Meta:
         model = User
-        #fileds = ["username", "password", "first_name", "last_name", "email",]
-        exclude=["last_login", "user_permissions","is_staff","is_active","date_joined","groups","is_superuser"]
+        fields = ("username", "email", "password")
+        #exclude=["last_login", "user_permissions","is_staff","is_active","date_joined","groups","is_superuser"]
     
 
+class UserUpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name")
 
+class CustomerUpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = ("phone", "address", )
--- food_delivery/apps/core_app/views.py
+++ food_delivery/apps/core_app/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-
-from core_app.forms import UserRegistrationForm
+from django.db import transaction
+from core_app.forms import UserRegistrationForm, UserUpdateProfileForm, CustomerUpdateProfileForm
 from core_app.models import Customer
-
+import django.contrib.messages
 
 def home(request):
     return render(request, 'core_app/home.html', {})
@@ -40,5 +40,21 @@ def logout_required(view):
 
 
 @login_required(login_url='/sign-in')
+@transaction.atomic
 def account(request):
-    return render(request, 'core_app/account.html', {})
+    if request.method == "POST":
+        user_profile_form = UserUpdateProfileForm(request.POST,instance=request.user)
+        customer_profile_form = CustomerUpdateProfileForm(request.POST,instance=request.user.customer)
+        if user_profile_form.is_valid() and customer_profile_form.is_valid():
+            user_profile_form.save()
+            customer_profile_form.save()
+            return redirect("core_app:account")
+        else:
+            messages.error(request, _('Исправьте ошибки!'))
+    else:
+        user_profile_form = UserUpdateProfileForm(instance=request.user)
+        customer_profile_form = CustomerUpdateProfileForm(instance=request.user.customer)
+    return render(request, 'core_app/account.html', {
+                "user_profile_form": user_profile_form,
+                "customer_profile_form":customer_profile_form
+                })
