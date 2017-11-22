9c9282a6a883475ff1536dbfae7c1e0bfe57b88b
Roman <sektor_wins@mail.ru>
2017-11-22 19:08:39 +0500

    В админ-панель добавлены миниатюры изображений блюд

--- food_delivery/apps/core_app/admin.py
+++ food_delivery/apps/core_app/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from .models import Meal, Order, ComplexDinner, Customer
+from django.utils.html import format_html
+
 
 admin.site.register(Order)
 admin.site.register(Customer)
 
 @admin.register(Meal)
 class MealAdmin(admin.ModelAdmin):
-    list_display = ('meal_name', 'category', 'cost', 'dinner',)
     list_filter = ('category',)
+    list_display = ('meal_name', 'category', 'cost', 'dinner', 'meal_image_thumbnail',)
+
 
 @admin.register(ComplexDinner)
 class ComplexDinnerAdmin(admin.ModelAdmin):
