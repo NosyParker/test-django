b941514e23f66532c5831f2eb1355901925de378
Roman <sektor_wins@mail.ru>
2017-11-22 17:36:12 +0500

    Upd: дополнены модели

--- food_delivery/apps/core_app/models.py
+++ food_delivery/apps/core_app/models.py
@@ -1,14 +1,13 @@
-# -*- coding: utf-8 -*-
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class ComplexDinner(models.Model):
     dinner_name = models.CharField(max_length=140, verbose_name="Название")
     cost = models.IntegerField(default=0, verbose_name="Цена")
     description = models.TextField(verbose_name="Описание")
-    dinner_img = models.ImageField(upload_to="dinners_images/", blank=False)
-
+    dinner_img = models.ImageField(upload_to="dinners_images/", verbose_name="Изображение", blank=False)
 
     def __str__(self):
         return self.dinner_name
@@ -29,12 +28,12 @@ class Meal(models.Model):
         ("MAIN_DISHES", "Вторые блюда"),
         ("DESSERTS", "Десерты"),
     )
-    dinner = models.ForeignKey(ComplexDinner, verbose_name="Входит в состав обеда", blank=True)
+    dinner = models.ForeignKey(ComplexDinner, verbose_name="Входит в состав обеда", blank=True, null=True)
     meal_name = models.CharField(max_length=140, verbose_name="Название")
     description = models.TextField(verbose_name="Описание")
     cost = models.IntegerField(default=0, verbose_name="Цена")
-    category = models.CharField(choices=MEAL_CATEGORY, max_length=20)
-    meal_img = models.ImageField(upload_to="meals_images/", blank=False)
+    category = models.CharField(choices=MEAL_CATEGORY, max_length=20, verbose_name="Категория")
+    meal_img = models.ImageField(upload_to="meals_images/", verbose_name="Изображение", blank=False)
 
 
     def __str__(self):
@@ -70,19 +69,32 @@ class Order(models.Model):
     customer = models.ForeignKey(Customer, verbose_name="Заказчик")
     status = models.IntegerField(choices=ORDER_STATUS, verbose_name="Статус заказа")
     address = models.CharField(max_length=500, verbose_name="Адрес")
+    created_at_time = models.DateTimeField(default = timezone.now, verbose_name="Время заказа")
+    delivered_at_time = models.DateTimeField(blank = True, null = True, verbose_name="Время доставки")
 
     def __str__(self):
         return str(self.id)
 
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
     class Meta:
+        ordering = ("-created_at_time",)
         verbose_name = "Заказ"
         verbose_name_plural = "Заказы"
 
 
 class OrderDetails(models.Model): 
-    order = models.ForeignKey(Order)
-    meal = models.ForeignKey(Meal)
-    quantity = models.IntegerField()
+    order = models.ForeignKey(Order, verbose_name="Заказ")
+    cost = models.IntegerField(null=True)
+    quantity = models.IntegerField(verbose_name="Количество")
 
     def __str__(self):
         return str(self.id)
+
+    def get_subtotal_cost(self):
+        return self.quantity * self.cost
+
+    class Meta:
+        verbose_name = "Детали заказа"
+        verbose_name_plural = "Детали заказа"
